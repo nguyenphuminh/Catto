89661c15a7176aba0a8f7b755a727ce49ceed0dd
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2024-07-17 12:42:14 +0700

    Reduce search depth

--- catto.config.js
+++ catto.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // General
-    searchDepth: 5,
+    searchDepth: 4,
     uci: true,
     fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
     version: "v0.5.2",
