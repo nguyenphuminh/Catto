e4c2493bc9f58a3e6a9629c38c3a8f0cbe44475d
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2024-08-27 19:28:08 +0700

    Add mate info in UCI client

--- catto.config.js
+++ catto.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Current version to show in UCI
-    version: "v0.12.1",
+    version: "v0.12.2",
     // Late move reduction config
     lmrFullDepth: 4, // Number of moves to be searched in full depth
     lmrMaxReduction: 3, // Only apply LMR above this depth
